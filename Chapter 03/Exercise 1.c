7806b3d7c615ebbd1f5b07b66b4469b169ec4508
Arc1el <khm970514@gmail.com>
2019-05-21 13:21:07 +0900

    Create Exercise 1.c

---
+++ Chapter 03/Exercise 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main ()
+{
+	int a; //정수형 변수인 int를 변수이름으로 선언불가
+	int ymca_2010; //변수이름 처음으로 숫자가 올 수 없음
+	int freeLec;
+	int aA; //변수이름에 공백이 올 수 없음
+	int howTo=20;
+
+	printf("HowTo = %d", howTo);
+
+	//int howTo=20; 변수선언은 호출전에
+	//int one+two; 변수이름에 기호는 _빼고 사용불가
+}
